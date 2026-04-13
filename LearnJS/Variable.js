77e7f62d3a746e19b1398e552d95c96c60edd2e7
[tanu sirohi] <[tanusirohi0604@gmai.com]>
2026-04-13 13:06:45 +0530

    Learn JS

---
+++ LearnJS/Variable.js
@@ -0,0 +1,40 @@
+// Let declare a variable.
+
+let a= 10;
+b= 20;
+ z = a+b;
+ console.log("Addition of a+b:", z);
+
+ // Now declare a variable with var keyword.
+ var c = 11;
+ d = 12;
+ var sum = c+d;
+ console.log("Addition:", sum);
+
+ // now declare a variable with keyword const.
+ const pi= 3.14;
+ console.log("Cost avlue is:", pi);
+
+ //JavaScript keywords are case-sensitive.
+
+//JavaScript does not interpret LET or Let as the keyword let.
+
+let Username = 'raj';
+let username = 'raj';
+let USERNAME =  'raj';
+let userName = 'raj';
+
+console.log( Username, userName, USERNAME, username)
+
+
+let CarName;
+CarName = "Maruti Suzuki"; //To assign a value to the variable, use the equal sign:
+console.log("My car name is:", CarName);
+
+//One Statement, Many Variables
+// You can declare many variables in one statement.
+// Start the statement with let or constand separate the variables by comma
+
+let Uname= 'Rani' , Age = 25 , City = "Agra" ;
+console.log("Uname =", Uname , "Age :", Age , "City :" , City);
+
